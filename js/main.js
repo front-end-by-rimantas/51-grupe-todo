b50584550ad1ab425a7ca2c1db4cb3c57c3c42c0
Rimantas <belauzas@gmail.com>
2024-09-04 12:01:50 +0300

    Click me

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-console.log('Labas rytas, Lietuva!');
+const allTaskDOMs = document.querySelectorAll('.task');
+
+for (const taskDOM of allTaskDOMs) {
+    const moreDOM = taskDOM.querySelector('.more');
+    const moreActionsDOM = taskDOM.querySelector('.more-actions');
+
+    if (moreDOM) {
+        moreDOM.addEventListener('click', () => {
+            moreActionsDOM.dataset.visible = 'true';
+        });
+    }
+}
